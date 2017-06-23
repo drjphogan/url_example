7fa7ce57e0ac2e74684a87b867b123a6a94033c5
John <dr.j.p.hogan@gmail.com>
2017-06-23 16:16:45 +0100

    added 3

--- test/test1.py
+++ test/test1.py
@@ -22,7 +22,7 @@ def print_items(items):
 def main(url):
     # comment to test push
     # another comment to test push
-    # another comment to test push
+    # another comment to test push 3
     words = fetch_words(url)
     print_items(words)
 

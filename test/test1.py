ea13fc0bf6c333116155bad49106dc769cbc018d
John <dr.j.p.hogan@gmail.com>
2017-06-23 12:57:17 +0100

    Initial commit

---
+++ test/test1.py
@@ -0,0 +1,29 @@
+import sys
+
+from urllib.request import urlopen
+
+
+def fetch_words(url):
+    with urlopen(url) as story:
+        story_words = []
+        for line in story:
+            #line_words = line.split()                  # as bytes
+            line_words = line.decode('utf-8').split()   # as strings
+            for word in line_words:
+                story_words.append(word)
+    return story_words
+
+
+def print_items(items):
+    for item in items:
+        print(item)
+
+
+def main(url):
+    words = fetch_words(url)
+    print_items(words)
+
+
+if __name__ == "__main__":
+    #'http://sixty-north.com/c/t.txt'
+    main(sys.argv[1])
